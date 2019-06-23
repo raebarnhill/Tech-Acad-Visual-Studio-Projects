a4f807f796f06cc1766115c3a9d72b9aa3383a8c
Rae <rae@gmail.com>
2019-06-22 18:22:27 -0700

    Create Student Daily Report

---
+++ Visual-Studio-Projects/Student Daily Report/Student Daily Report/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            Console.ReadLine();
+            bool needsHelp = true;
+            string helpStatus = Convert.ToString(needsHelp);
+            Console.WriteLine("Were there any positive expereinces you'd like to share? Please give specifics.");
+            string posExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            Console.ReadLine();
+            int studyHours = 25;
+            string hoursStudied = Convert.ToString(studyHours);
+            Console.WriteLine("Thank you for your answers. An instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
