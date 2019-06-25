f7feea55610d83db53b7867d5fda1e1b70e799ec
Rae <rae@gmail.com>
2019-06-24 18:05:13 -0700

    Create Shipping Quote Program
    Assignment pg 92

---
+++ Shipping Quote Program/Shipping Quote Program/Shipping Quote Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_Quote_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the weight of your package:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a nice day.");
+             
+            }
+            else if (packageWeight <= 50)
+            {
+                Console.WriteLine("Please enter the package width:");
+            }
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your estimated total for shipping this package is:");
+            int quote = (packageWidth + packageHeight + packageLength) * packageWeight;
+            float finalQuote = (float)quote / 100;
+
+            Console.WriteLine("$" + finalQuote);
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
