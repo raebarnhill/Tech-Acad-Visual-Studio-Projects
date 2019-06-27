0c8a7479f4553c0f4e65b4dec3075cb3823048aa
Rae <rae@gmail.com>
2019-06-27 04:24:47 -0700

    Fix DUI bool

--- Car Insurance Qualification/Car Insurance Program/Car Insurance Program/Program.cs
+++ Car Insurance Qualification/Car Insurance Program/Car Insurance Program/Program.cs
@@ -12,17 +12,15 @@ namespace Car_Insurance_Program
         {
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI? Please enter true or false.");
-            bool hadDUI = true;
-            string DUI = Console.ReadLine();
 
-            
+            Console.WriteLine("Have you ever had a DUI? Please enter true or false.");
+            bool hadDUI = Convert.ToBoolean(Console.ReadLine());
             
 
             Console.WriteLine("How many speeding tickets do you have?");
             int numTickets = Convert.ToInt32(Console.ReadLine());
 
-            bool isQualified = (age > 15 && hadDUI && numTickets <= 3);
+            bool isQualified = !(hadDUI || age <=15 || numTickets > 3);
 
             Console.WriteLine("Qualified?");
             Console.WriteLine(isQualified);
