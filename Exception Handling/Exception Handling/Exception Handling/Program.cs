fa64c8d81487b6c9fea4f48c32ee021038761633
Rae <rae@gmail.com>
2019-07-02 04:21:23 -0700

    Create try/catch block
    Assignment pg 106

---
+++ Exception Handling/Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a list of integers. Ask the user for a number to divide each 
+            //number in the list by.Write a loop that takes each integer in the list,
+            //divides it by the number the user entered, and displays the result to the
+            //screen.
+
+            //List<int> numbers = new List<int>
+            //{
+            //    20,
+            //    100,
+            //    50
+            //};
+
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine("Please enter a number:");
+            //    int divisor = Convert.ToInt32(Console.ReadLine());
+            //    int number1 = numbers[0] / divisor;
+            //    Console.WriteLine("20 divided by your number equals: " + number1);
+
+            //    int number2 = numbers[1] / divisor;
+            //    Console.WriteLine("100 divided by your number is: " + number2);
+
+            //    int number3 = numbers[2] / divisor;
+            //    Console.WriteLine("50 divided by your number is: " + number3);
+            //    Console.ReadLine();
+            //}
+
+
+
+            // 5.Now put the loop in a try/catch block.Below and outside of the try/catch block, 
+            //make the program display a message to the display to let you know the program has 
+            //emerged from the try/catch block and continued on with program execution.In the catch 
+            //block, display the error message to the screen.Then try various combinations of user input: 
+            //valid numbers, zero and a string.Ensure the proper error messages display on the screen, and 
+            //that the code after the try/catch block gets executed.
+
+
+            List<int> numbers = new List<int>
+            {
+                20,
+                100,
+                50
+            };
+
+
+            try
+            {
+                Console.WriteLine("Please enter a number:");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                int number1 = 20 / divisor;
+                Console.WriteLine("20 divided by your number is: " + number1);
+                int number2 = 100 / divisor;
+                Console.WriteLine("100 divided by your number is: " + number2);
+                int number3 = 50 / divisor;
+                Console.WriteLine("50 divided by your number is: " + number3);
+                Console.ReadLine();
+            }
+
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+          
+            finally
+            {
+                Console.WriteLine("Exited the try/catch block.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Loops/Loops and Lists/Loops and Lists/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1.Create a one-dimensional Array of strings.Ask the user to
+            //input some text.Create a loop that goes through each string
+            //in the Array, adding the user’s text to the string.Then 
+            //create a loop that prints off each string in the Array on
+            //a separate line.
+
+
+            //Console.WriteLine("Please enter your favorite animal:");
+            //string favAnimal = Console.ReadLine();
+            //string[] animals = { "cat ", "butterfly ", "zebra ", favAnimal };
+
+
+            //foreach (string animal in animals)
+            //{
+            //    Console.WriteLine(animal);
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //2. Create an infinite loop
+
+
+
+            //for (; ;)
+            //{
+            //    Console.WriteLine("Oops..");
+            //}
+
+
+            //3. Fix the infinite loop so it will execute
+            //4.Create a loop where the comparison used to determine whether to continue 
+            //iterating the loop is a “<” operator.
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Some pretty colors:");
+            //}
+            //Console.ReadLine();
+
+
+            //5.Create a loop where the comparison used to determine whether to continue 
+            //iterating the loop is a “<=” operator.
+
+            //int x = 1;
+
+            //while (x <= 10)
+            //{
+            //    Console.WriteLine("iterate");
+            //    x++;
+            //}
+            //Console.ReadLine
+
+
+            //6.Create a List of strings where each item in the list is unique.Ask the user 
+            //to select text to search for in the List.Create a loop that iterates through 
+            //the list and then displays the index of the array that contains matching text 
+            //on the screen.
+            //7.Add code to that above loop that tells a user if they put in text that isn’t in the List.
+            //8.Add code to that above loop that stops it from executing once a match has been found.
+
+            //List<string> colors = new List<string>();
+            //colors.Add("Red");
+            //colors.Add("Green");
+            //colors.Add("Blue");
+
+            //Console.WriteLine("Please enter one of the following: Red, Green or Blue:");
+            //string colorChoice = Console.ReadLine();
+            //string color = colorChoice;
+            //bool colorGuess = false;
+
+            //while (!colorGuess)
+
+            //switch (color)
+            //    {
+            //        case "Red":
+            //        int index = colors.IndexOf("Red");
+            //        Console.WriteLine("You chose the color " + colors[0] + " from index: " + index);
+            //            colorGuess = true;
+            //            break;
+
+            //        case "Green":
+            //        index = colors.IndexOf("Green");
+            //        Console.WriteLine("You chose the color " + colors[1] + " from index: " + index);
+            //            colorGuess = true;
+            //            break;
+
+            //        case "Blue":
+            //        index = colors.IndexOf("Blue");
+            //        Console.WriteLine("You chose the color " + colors[2] + " from index: " + index);
+            //            colorGuess = true;
+            //            break;
+
+
+            //        default:
+            //            Console.WriteLine("Please enter a valid color:");
+            //            color = (Console.ReadLine());
+            //            break;
+            //    }
+
+
+            //Console.ReadLine();
+
+
+
+
+            //9.Create a List of strings that has at least two identical strings in the List. Ask the user to 
+            //select text to search for in the List.Create a loop that iterates through the list and then displays 
+            //the indices of the array that contain matching text on the screen.
+            //10.Add code to that above loop that tells a user if they put in text that isn’t in the List.
+
+            //List<string> animals = new List<string>() { "cat", "cat", "dog", "dog", "cow", "horse", "fish" };
+
+
+            //Console.WriteLine("Please enter a type of animal:");
+            //string animalChoice = Console.ReadLine();
+
+            //for (int i = 0; i < animals.Count; i++)
+            //{
+            //    if (animalChoice == animals[i])
+            //    {
+            //        Console.WriteLine("You've chosen the animal " + animalChoice + " at index " + i);
+
+            //    }
+
+            //    else 
+            //    {
+            //        Console.WriteLine("The animal you've chosen isn't in the array.");
+
+            //    }
+            //    Console.ReadLine();
+
+            //}
+
+
+            //11.Create a List of strings that has at least two identical strings in the List. Create a foreach loop that 
+            //evaluates each item in the list, and displays a message showing the string and whether or not it has already 
+            //appeared in the list.
+
+            List<string> names = new List<string>() { "Charlie", "Evan", "Abi", "Abi", "Daniel", "Daniel" };
+
+            foreach (string name in names.GroupBy(x => x))
+            {
+                Console.WriteLine(names + " repeats " + (name.Count() - 1) + " times");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+        }
+    
+
+
+   
+
+
+
+
+   
