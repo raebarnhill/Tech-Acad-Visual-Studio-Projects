7ebca20061265575cec0720fb8bd09fbe26dd8a4
Rae <rae@gmail.com>
2019-07-06 14:33:47 -0700

    Create Void Method
    Assignment pg 113

---
+++ Void Method/Void Method/Void Method/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Car
+    {
+       
+        public void TopSpeed(int carSpeed = 80, int speedLimit = 25)
+        {
+
+            int overLimit = carSpeed - speedLimit;
+            Console.WriteLine(speedLimit);
+             return;
+        }
+
+
+
+        public Car()
+        {
+            List<Type> Types = new List<Type>();
+            List<string> Makes = new List<string>() { "Honda", "Geo", "Toyota" };
+            List<string> Models = new List<string>() { "Civic", "Metro", "Yaris" };
+
+
+
+            foreach (string make in Makes)
+            {
+                foreach (string model in Models)
+                {
+                    Type type = new Type();
+                    type.Make = make;
+                    type.Model = model;
+                    Types.Add(type);
+                }
+
+            }
+
+        }
+
+    }
+}
+
---
+++ Void Method/Void Method/Void Method/Type.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    public class Type
+    {
+        public Type()
+        {
+            Make = "Honda";
+            Model = "Civic";
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
+
+
+
+
