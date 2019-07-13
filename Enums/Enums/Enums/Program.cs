fccf708dca8b016783cc686c858f78ab0ee588ff
Rae <rae@gmail.com>
2019-07-13 06:55:04 -0700

    Create Struct Number
    Assignment pg 137

---
+++ Enums/Enums/Enums/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Create an enum for the days of the week.
+
+//2. Prompt the user to enter the current day of the week.
+
+//3. Assign the value to a variable of that enum data type 
+//you just created.
+
+//4. Wrap the above statement in a try/catch block and have it print 
+//"Please enter an actual day of the week." to the console if an error occurs.
+
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week:");
+            Console.ReadLine();
+
+            DaysOfTheWeek monday = DaysOfTheWeek.Monday;
+            DaysOfTheWeek tuesday = DaysOfTheWeek.Tuesday;
+            DaysOfTheWeek wednesday = DaysOfTheWeek.Wednesday;
+            DaysOfTheWeek thursday = DaysOfTheWeek.Thursday;
+            DaysOfTheWeek friday = DaysOfTheWeek.Friday;
+            DaysOfTheWeek saturday = DaysOfTheWeek.Saturday;
+            DaysOfTheWeek sunday = DaysOfTheWeek.Sunday;
+            
+
+
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+
+
+
+    }
+
+
+}
+
---
+++ Structs/Structs/Structs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Create a struct called Number and give it the property "Amount" 
+//and have it be of data type decimal.
+
+//2. In the Main() method, create an object of data type Number and 
+//assign an amount to it.
+
+//3. Print this amount to the console.
+
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1 = new Number();
+            number1.Amount = 100.5m;
+
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
