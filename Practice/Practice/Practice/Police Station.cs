7024e130024df60c376a8e87f6b690e703c9e343
Rae <rae@gmail.com>
2019-07-18 16:22:03 -0700

    Practice
    Create const variable, chain constructors,  variable with var pg 162

---
+++ Practice/Practice/Practice/Police Station.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+   public class Police_Station
+    {
+        public Police_Station() : this(2000)
+        {
+
+        }
+        public Police_Station(int total)
+        {
+            
+        }
+
+        public int Total { get; set; }
+        
+    }
+}
---
+++ Practice/Practice/Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Create a const variable
+
+//2. Create a variable using the keyword "var".
+
+//3. Chain two constructors together.
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string racoonCity = "Raccoon City PD";
+            
+            var officer1 = "Helena";
+
+            Console.WriteLine("Leon and " + officer1 + " are officers with the {0}.", racoonCity);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
